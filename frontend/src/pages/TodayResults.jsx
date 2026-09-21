18d533b86c40a6b64a84d5000f20c2f014815390
odongotim <timothyodongo263@gmail.com>
2026-09-21 16:55:28 +0300

    Update TodayResults.jsx

--- frontend/src/pages/TodayResults.jsx
+++ frontend/src/pages/TodayResults.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+{/*import React, { useEffect, useState } from 'react';
 import { api, ApiError } from '../api/api.js';
 import ErrorMessage from '../components/ErrorMessage.jsx';
 import PieChart from '../components/PieChart.jsx';
@@ -56,3 +56,4 @@ export default function TodayResults() {
     </div>
   );
 }
+*/}
